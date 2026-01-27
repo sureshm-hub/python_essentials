915faf247ec5a75377af4c65f389b7247d215278
sures <suresh.malladi@gmail.com>
2026-01-26 20:57:32 -0600

    power of three

---
+++ proj/hobby/dsa/math/PowerOfThree.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/power-of-three
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n > 1:
+            if n % 3 != 0:
+                return False
+            n /= 3
+
+        return True
