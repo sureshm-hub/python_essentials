65447d73f4e964e31c1b92e92cb8278156bfa1e7
sures <suresh.malladi@gmail.com>
2026-01-27 20:00:47 -0600

    Odd Even

---
+++ proj/hobby/dsa/linkedlist/OddEven.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/odd-even-linked-list
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd = ListNode()
+        temp_odd = odd
+        even = ListNode()
+        temp_even = even
+        cur = head
+        idx = 1
+        while cur:
+            if idx % 2 == 1:
+                temp_odd.next = ListNode(cur.val)
+                temp_odd = temp_odd.next
+            else:
+                temp_even.next = ListNode(cur.val)
+                temp_even = temp_even.next
+            cur = cur.next
+            idx += 1
+
+        temp_odd.next = even.next
+        return odd.next
+
+
+
+
